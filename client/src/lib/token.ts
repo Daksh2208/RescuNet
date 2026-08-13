8cca159df07699c2b37f8db9cf91fd7d99edad01
Dipal Rana <rdipal993@gmail.com>
2026-08-13 13:49:47 +0530

    Resolved minor bug

--- client/src/lib/token.ts
+++ client/src/lib/token.ts
@@ -1,21 +1,44 @@
-let accessToken = localStorage.getItem("accessToken") || "";
+// let accessToken = localStorage.getItem("accessToken") || "";
+
+// export const setAccessToken = (token: string) => {
+
+//     accessToken = token;
+
+//     if (token) {
+
+//         localStorage.setItem("accessToken", token);
+
+//     } else {
+
+//         localStorage.removeItem("accessToken");
+
+//     }
+
+// };
+
+// export const getAccessToken = () => {
+//     return accessToken;
+// };
+
+let accessToken = "";
 
 export const setAccessToken = (token: string) => {
+  accessToken = token;
 
-    accessToken = token;
-
+  if (typeof window !== "undefined") {
     if (token) {
-
-        localStorage.setItem("accessToken", token);
-
+      localStorage.setItem("accessToken", token);
     } else {
-
-        localStorage.removeItem("accessToken");
-
+      localStorage.removeItem("accessToken");
     }
-
+  }
 };
 
 export const getAccessToken = () => {
-    return accessToken;
+  // Restore token from browser storage when running on client
+  if (typeof window !== "undefined" && !accessToken) {
+    accessToken = localStorage.getItem("accessToken") || "";
+  }
+
+  return accessToken;
 };
